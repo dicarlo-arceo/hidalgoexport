7e90117449059adffbc83842b0d89efb5fb90cf4
cheloynesh <cheloynesh@gmail.com>
2021-08-20 09:43:21 -0500

    Cambio de titulo a Trazenta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title','Sistema') | ELAN</title>
+    <title>@yield('title','Sistema') | Trazenta</title>
 
     {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
 
