5298b3b4181d68d508fe32aa7ebe863ffc2e28df
Chelo <dicarloarceo@gmail.com>
2021-09-07 18:54:45 -0500

    arreglos esteticos a permisos

--- resources/views/admin/permission/permissions.blade.php
+++ resources/views/admin/permission/permissions.blade.php
@@ -22,118 +22,117 @@
         </div>
         {{-- inicia panel de permisos  --}}
 {{-- {!! Form::open(['route'=> ['admin.permission.update_store',':USER_ID'],'method'=>'POST', 'id'=>'form-update_store']) !!} --}}
+    </div>
+</div>
+&nbsp
+<div id="accordion">
+    @foreach($padre as $padres )
 
-        <div class="panel-group" id="accordion">
-            @foreach($padre as $padres )
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse{{$padres->id}}" data-padre="{{$padre = $padres->id}}">{{$padres->section}}</a>
+                </h4>
+            </div>
+            <div id="collapse{{$padres->id}}" class="collapse">
+                <div class="card-body">
+                    <div class="table-responsive" style="margin: 10px auto; max-width: 1200px">
+                        <table class="table table-striped table-hover text-center">
+                            <thead >
+                            <th class="text-center">Modulo</th>
+                            <th class="text-center">Ver</th>
+                            <th class="text-center">Agregar</th>
+                            <th class="text-center">Editar</th>
+                            <th class="text-center">Eliminar</th>
+                            </thead>
+                            <tbody>
+                            @foreach($hijos as $hijo )
 
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <h4 class="panel-title">
-                            <a data-toggle="collapse" data-parent="#accordion" href="#collapse{{$padres->id}}" data-padre="{{$padre = $padres->id}}">{{$padres->section}}</a>
-                        </h4>
-                    </div>
-                    <div id="collapse{{$padres->id}}" class="panel-collapse collapse ">
-                        <div class="panel-body">
+                                @if($hijo->reference == $padres->id)
 
-                            <div class="table-responsive" style="margin: 10px auto; max-width: 1200px">
-                                <table class="table table-striped table-hover text-center">
-                                    <thead >
-                                    <th class="text-center">Modulo</th>
-                                    <th class="text-center">Ver</th>
-                                    <th class="text-center">Agregar</th>
-                                    <th class="text-center">Editar</th>
-                                    <th class="text-center">Eliminar</th>
-                                    </thead>
-                                    <tbody>
-                                    @foreach($hijos as $hijo )
-
-                                        @if($hijo->reference == $padres->id)
-
-                                            <tr data-hijo="{{$hijo->id}}" data-reference="{{$hijo->reference}}">
-                                                <td class="text-left">
-                                                    <strong>
-                                                        {{$hijo->section}}
-                                                    </strong>
-                                                </td>
-                                                <!--VALIDAR-->
-                                                @if($hijo->hijo==0)
-                                                    <td>
-                                                        <input type="checkbox"   id="view_{{$hijo->id}}" disabled="disabled" class="view_id" onclick="clickView({{$hijo->id}},{{$hijo->reference}})">
-                                                    </td>
-                                                    <td>
-                                                        @if($hijo->haveAdd==1)
-                                                            <input type="checkbox" id="add_{{$hijo->id}}" disabled="disabled" class="add_id" onclick="clickAdd({{$hijo->id}},{{$hijo->reference}})">
-                                                        @endif
-                                                    </td>
-                                                    <td>
-                                                        @if($hijo->haveUpdate==1)
-                                                            <input type="checkbox" id="update_{{$hijo->id}}" disabled="disabled" class="update_id" onclick="clickUpdate({{$hijo->id}},{{$hijo->reference}})">
-                                                        @endif
-                                                    </td>
-                                                    <td>
-                                                        @if($hijo->haveDelete==1)
-                                                            <input type="checkbox" id="delete_{{$hijo->id}}" disabled="disabled" class="delete_id" onclick="clickDelete({{$hijo->id}},{{$hijo->reference}})">
-                                                        @endif
-                                                    </td>
-                                                @else
-                                                    <td></td>
-                                                    <td></td>
-                                                    <td></td>
-                                                    <td></td>
-                                            </tr>
-                                            @endif
-                                            <!--/VALIDAR-->
-
-                                            @foreach($hijos as $hijo2 )
-
-                                                @if($hijo2->reference == $hijo->id)
-                                                    <tr data-hijo="{{$hijo2->id}}" data-reference="{{$hijo2->reference}}">
-                                                        <td class="text-left">
-                                                                   <span style="padding-left: 20px;">
-                                                                       {{$hijo2->section}}
-                                                                   </span>
-                                                        </td>
-                                                        <td >
-                                                            <input type="checkbox" id="view_{{$hijo2->id}}" disabled="disabled" class="view_id" onclick="clickView({{$hijo2->id}},{{$hijo->reference}})">
-                                                        </td>
-                                                        <td>
-                                                            @if($hijo2->haveAdd==1)
-                                                                <input type="checkbox" id="add_{{$hijo2->id}}" disabled="disabled"  class="add_id" onclick="clickAdd({{$hijo2->id}},{{$hijo->reference}})">
-                                                            @endif
-                                                        </td>
-                                                        <td>
-                                                            @if($hijo2->haveUpdate==1)
-                                                                <input type="checkbox" id="update_{{$hijo2->id}}" disabled="disabled" class="update_id" onclick="clickUpdate({{$hijo2->id}},{{$hijo->reference}})">
-                                                            @endif
-                                                        </td>
-                                                        <td>
-                                                            @if($hijo2->haveDelete==1)
-                                                                <input type="checkbox" id="delete_{{$hijo2->id}}" disabled="disabled" class="delete_id" onclick="clickDelete({{$hijo2->id}},{{$hijo->reference}})">
-                                                            @endif
-                                                        </td>
-                                                    </tr>
+                                    <tr data-hijo="{{$hijo->id}}" data-reference="{{$hijo->reference}}">
+                                        <td class="text-left">
+                                            <strong>
+                                                {{$hijo->section}}
+                                            </strong>
+                                        </td>
+                                        <!--VALIDAR-->
+                                        @if($hijo->hijo==0)
+                                            <td>
+                                                <input type="checkbox"   id="view_{{$hijo->id}}" disabled="disabled" class="view_id" onclick="clickView({{$hijo->id}},{{$hijo->reference}})">
+                                            </td>
+                                            <td>
+                                                @if($hijo->haveAdd==1)
+                                                    <input type="checkbox" id="add_{{$hijo->id}}" disabled="disabled" class="add_id" onclick="clickAdd({{$hijo->id}},{{$hijo->reference}})">
                                                 @endif
+                                            </td>
+                                            <td>
+                                                @if($hijo->haveUpdate==1)
+                                                    <input type="checkbox" id="update_{{$hijo->id}}" disabled="disabled" class="update_id" onclick="clickUpdate({{$hijo->id}},{{$hijo->reference}})">
+                                                @endif
+                                            </td>
+                                            <td>
+                                                @if($hijo->haveDelete==1)
+                                                    <input type="checkbox" id="delete_{{$hijo->id}}" disabled="disabled" class="delete_id" onclick="clickDelete({{$hijo->id}},{{$hijo->reference}})">
+                                                @endif
+                                            </td>
+                                        @else
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                    </tr>
+                                    @endif
+                                    <!--/VALIDAR-->
 
-                                            @endforeach
+                                    @foreach($hijos as $hijo2 )
 
+                                        @if($hijo2->reference == $hijo->id)
+                                            <tr data-hijo="{{$hijo2->id}}" data-reference="{{$hijo2->reference}}">
+                                                <td class="text-left">
+                                                            <span style="padding-left: 20px;">
+                                                                {{$hijo2->section}}
+                                                            </span>
+                                                </td>
+                                                <td >
+                                                    <input type="checkbox" id="view_{{$hijo2->id}}" disabled="disabled" class="view_id" onclick="clickView({{$hijo2->id}},{{$hijo->reference}})">
+                                                </td>
+                                                <td>
+                                                    @if($hijo2->haveAdd==1)
+                                                        <input type="checkbox" id="add_{{$hijo2->id}}" disabled="disabled"  class="add_id" onclick="clickAdd({{$hijo2->id}},{{$hijo->reference}})">
+                                                    @endif
+                                                </td>
+                                                <td>
+                                                    @if($hijo2->haveUpdate==1)
+                                                        <input type="checkbox" id="update_{{$hijo2->id}}" disabled="disabled" class="update_id" onclick="clickUpdate({{$hijo2->id}},{{$hijo->reference}})">
+                                                    @endif
+                                                </td>
+                                                <td>
+                                                    @if($hijo2->haveDelete==1)
+                                                        <input type="checkbox" id="delete_{{$hijo2->id}}" disabled="disabled" class="delete_id" onclick="clickDelete({{$hijo2->id}},{{$hijo->reference}})">
+                                                    @endif
+                                                </td>
+                                            </tr>
                                         @endif
 
                                     @endforeach
 
-                                    </tbody>
-                                </table>
+                                @endif
 
-                                {{-- {!! Form::close() !!} --}}
+                            @endforeach
 
-                            </div>
-                        </div>
+                            </tbody>
+                        </table>
+
+                        {{-- {!! Form::close() !!} --}}
+
                     </div>
                 </div>
-
-            @endforeach
-
+            </div>
         </div>
-    </div>
+
+    @endforeach
+
 </div>
 
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -101,7 +101,7 @@
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     {{-- <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script> --}}
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    {{-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> --}}
     <!-- Popper.JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <!-- Bootstrap JS -->
