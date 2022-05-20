d4b41cd460acb22dd7054941bbe9ad6e07b3c33a
Dicarlo <dicarloarceo@gmail.com>
2022-05-19 20:54:13 -0500

    base proyecto funcionando

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="description" content="Trazenta">
-    <title>@yield('title','Sistema') | Trazenta</title>
+    <meta name="description" content="Hidalgo Export">
+    <title>@yield('title','Sistema') | Hidalgo Export</title>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Fonts -->
--- routes/web.php
+++ routes/web.php
@@ -34,48 +34,9 @@ Route::resource('admin/pruebas/prueba', 'PruebasController');
 Route::resource('admin/users/user', 'UsersController');
 Route::get('admin/users/user/GetInfo/{id}','UsersController@GetInfo')->name('user.GetInfo');
 
-// clientes
-Route::resource('admin/client/client', 'ClientsController');
-Route::get('admin/client/client/GetInfo/{id}','ClientsController@GetInfo')->name('client.GetInfo');
-Route::post('admin/client/client/SaveNuc','ClientsController@SaveNuc')->name('client.SaveNuc');
-
 // permisos
 Route::resource('admin/permission/permissions', 'PermissionsController');
 Route::get('admin/permission/permissions/{id}/{id_seccion?}/{btn?}/{reference?}',[ 'uses' => 'PermissionsController@update_store', 'as' => 'admin.permission.update_store']);
 Route::post('admin/permission/permissions/update_store','PermissionsController@update_store')->name('permissions.update_store');
-
-// ------------------------------------------ Procesos de fondos --------------------------------------------------
-// fondo mensual
-Route::resource('funds/monthfund/monthfunds', 'MonthFundsController');
-Route::get('funds/monthfund/monthfunds/GetInfo/{id}','MonthFundsController@GetInfo')->name('monthfunds.GetInfo');
-Route::get('funds/monthfund/monthfunds/GetInfoLast/{id}','MonthFundsController@GetInfoLast')->name('monthfunds.GetInfoLast');
-Route::post('funds/monthfund/monthfunds/updateStatus', 'MonthFundsController@updateStatus')->name('monthfunds.updateStatus');
-Route::post('funds/monthfund/monthfunds/updateAuth', 'MonthFundsController@updateAuth')->name('monthfunds.updateAuth');
-Route::get('funds/monthfund/monthfunds/GetNuc/{id}','MonthFundsController@GetNuc')->name('monthfunds.GetNuc');
-Route::get('funds/monthfund/monthfunds/ExportFunds/{id}','MonthFundsController@ExportFunds');
-// Route::get('funds/monthfund/monthfunds/GetNuc/{id}','MonthFundsController@GetNuc')->name('monthfunds.GetNuc');
-
-// -------------------------------------------------- Asignación de clientes--------------------------------------------
-
-Route::resource('admin/assiment/assigment', 'AssigmentController');
-Route::get('admin/assiment/assigment/Viewclients/{id}','AssigmentController@Viewclients')->name('assigment.Viewclients');
-Route::post('admin/assiment/assigment/updateClient', 'AssigmentController@updateClient')->name('assigment.updateClient');
-
-//---------------------------------------------------Comisiones fondo mensual--------------------------------------------
-
-Route::resource('funds/monthlycomission/monthcomission', 'MonthComissionController');
-Route::get('funds/monthlycomission/monthcomission/GetInfo/{id}','MonthComissionController@GetInfo')->name('monthcomission.GetInfo');
-// Route::get('funds/monthlycomission/monthcomission/GetInfoMonth/{id}/{month}/{year}','MonthComissionController@GetInfoMonth')->name('monthcomission.GetInfoMonth');
-// Route::get('funds/monthlycomission/monthcomission/GetInfoLast/{id}/{month}/{year}','MonthComissionController@GetInfoLast')->name('monthcomission.GetInfoLast');
-Route::get('funds/monthlycomission/monthcomission/ExportPDF/{id}/{month}/{year}/{TC}/{regime}','MonthComissionController@ExportPDF');
-Route::post('funds/monthlycomission/monthcomission/GetInfoComition','MonthComissionController@GetInfoComition')->name('monthcomission.GetInfoComition');
-
-//---------------------------------------------------Comisiones 6 meses fondo mensual--------------------------------------------
-Route::resource('funds/fstmonthcomission/fstmonthcomission', 'FstMonthComissionController');
-Route::get('funds/fstmonthcomission/fstmonthcomission/GetInfo/{id}','FstMonthComissionController@GetInfo')->name('fstmonthcomission.GetInfo');
-Route::get('funds/fstmonthcomission/fstmonthcomission/ExportPDF/{id}/{month}/{year}/{TC}/{regime}','FstMonthComissionController@ExportPDF');
-Route::get('funds/fstmonthcomission/fstmonthcomission/ExportPDFAll/{id}/{month}/{year}/{TC}/{regime}','FstMonthComissionController@ExportPDFAll');
-Route::post('funds/fstmonthcomission/fstmonthcomission/GetInfoComition','FstMonthComissionController@GetInfoComition')->name('fstmonthcomission.GetInfoComition');
-
 
 
